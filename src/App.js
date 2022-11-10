8a6580a5fd2203f692a9185a888f54a410aed90f
sunnynotsunhee <maja.maric1331@gmail.com>
2022-11-10 14:52:26 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Deploying on Netlify</h1>
+        <h1>Second commit!</h1>
       </header>
     </div>
   );
